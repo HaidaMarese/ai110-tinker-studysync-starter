725a4ff4be274f3f3a300369d35870cbf4ef0bbf
Haida Makouangou <hmakouan06@gmail.com>
2026-09-20 07:39:11 -0400

    test: add session rating boundary test

--- test_scoring.py
+++ test_scoring.py
@@ -13,3 +13,6 @@ def test_session_rating_boundary_90_is_great():
 
 # TODO: add at least one more test, e.g. a boundary case for "Skip" (a score
 # of 59) or the exact boundary for "Good" (a score of 80).
+
+def test_session_rating_boundary_80_is_good():
+    assert session_rating(80) == "Good"
